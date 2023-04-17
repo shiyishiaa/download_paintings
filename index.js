61396577dd3d3170a2010f5f35a548f9c600f53e
Norman <yining@mail2.sysu.edu.cn>
2023-04-18 07:50:44 +0800

    finish the spider

--- index.js
+++ index.js
@@ -2,21 +2,43 @@ import puppeteer from "puppeteer";
 
 (async () => {
     // Launch the browser
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+        // headless: false,
+        slowMo: 250,
+        // devtools: true
+    });
 
     // Create a page
     const page = await browser.newPage();
 
-    // Go to your site
-    await page.goto('https://www.google.com');
+    /* Enquire max page */
+    const baseUrl = "https://www.metmuseum.org/art/collection/search?department=1&showOnly=highlights%7CwithImage&material=Paintings";
+    await page.goto(baseUrl);
+    const pagination = await page.$$eval("button.pagination-button", buttons => buttons.map(button => button.innerText));
+    const maxPage = Number(pagination[pagination.length - 2]);
 
-    // Evaluate JavaScript
-    const three = await page.evaluate(() => {
-        return 1 + 2;
-    });
+    /* Construct page urls */
+    const urls = []
+    for (let i = 0; i < maxPage; i++) {
+        urls.push(`${baseUrl}&offset=${i * 40}`)
+    }
 
-    console.log(three);
+    /* Acquire image thumbnail addresses */
+    let imageLinks = [];
+    for (let url of urls) {
+        await page.goto(url)
+        const images = await page.$$eval(".result-object__image-container > img", imgs => imgs.map(img => img.getAttribute("src")));
+        imageLinks = imageLinks.concat(images)
+    }
 
-    // Close browser.
+    /* Substitute thumbnail addresses to download links */
+    let pattern = /CRDImages\/(.*?)\/mobile-large/;
+    const abbr = imageLinks[0].match(pattern)[1];
+    const prefix = `https://images.metmuseum.org/CRDImages/${abbr}/original/`;
+    imageLinks.forEach(link => {
+        const fileName = link.replace(`https://images.metmuseum.org/CRDImages/${abbr}/mobile-large/`, "");
+        console.log(prefix + fileName);
+    })
+
     await browser.close();
 })();
